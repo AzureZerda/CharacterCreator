c5acc2d8052aa271652f58a4df2e89eaa9d07679
AzureFennec <carnauris@gmail.com>
2026-06-26 01:56:15 -0700

    Added prebuilts

---
+++ prebuilts.py
@@ -0,0 +1,162 @@
+PREBUILTS={
+    'Shield Fighter':{
+        'description':'Good frontline fighter.',
+        'required_equipment':'Shield, Sword(no longer than 48"), armor resembling plate mail. Remember to make sure everything can pass Twin Mask safety checks.',
+        'skills':{
+            "Clouded Memory": 1,
+            "Illiterate": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Armored Training: Light": 1,
+            "Armored Training: Heavy": 1,
+            "Shield Use": 1,
+            "Toughness": 1,
+            "Parry": 3,
+            "Great Strike": 2,
+            "Dodge": 1,
+            "Willpower": 1
+        }
+    },
+    'Raging Barbarian':{
+        'description':'Skirmisher that specializes in dealing lots of damage quickly. Remember to review Twin Mask rules on charging.',
+        'required_equipment':'1-2 swords. Armor resembling leather.',
+        'skills':{
+            "Clouded Memory": 1,
+            "Sovereign Zeal": 1,
+            "Illiterate": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Two-Weapon Fighting": 1,
+            "Armored Training: Light": 1,
+            "Battle Rage": 3,
+            "Enduring Rage": 1,
+            "Break Limb": 1,
+            "Toughness": 1,
+            "Leap": 1
+        }
+    },
+    'Knife-Wielding Assassin':{
+        'description':'Specializes in dealing a burst of energy and quickly escaping.',
+        'required_equipment':'A dagger no longer than 24"',
+        'skills':{
+            "Clouded Memory": 1,
+            "Illiterate": 1,
+            "Short Weapons": 1,
+            "Stealth Attack": 1,
+            "10-Damage Strike": 3,
+            "Stun": 1,
+            "Disarm": 1,
+            "Armored Forearms": 1,
+            "Leap": 1
+        }
+    },
+    'Stylish Duelist':{
+        'description':'Specializes in 1-on-1 combat. Please review the rules for SALUTE.',
+        'required_equipment':'A fancy hat. A sword up to 48"',
+        'skills':{
+            "Clouded Memory": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Salute": 1,
+            "Stylish Hat": 1,
+            "Witty Repartee": 1,
+            "Disarm": 2,
+            "Parry": 1,
+            "Dodge": 1,
+            "Dance Lesson": 1
+        }
+    },
+    'Wandering Monk':{
+        'description':'A little bit of everything. Basic healing, combat, and religious abilities. If literacy is important to you, drop illiteracy.',
+        'required_equipment':'Strips of fabric for bandages. A sword up to 48"',
+        'skills':{
+            "Clouded Memory": 1,
+            "Illiterate": 1,
+            "Bardic Knowledge": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Parry": 1,
+            "Willpower": 1,
+            "Armored Forearms": 1,
+            "Leap": 2,
+            "Break Limb": 1,
+            "Prayer": 1,
+            "Examine Wounds": 1,
+            "Apply Pressure": 1,
+            "Set Bone": 1,
+            "Bandage": 1
+        }
+    },
+    'Master Sorcerer':{
+        'description':'You can only do sorcery. But you are very good at sorcery.',
+        'required_equipment':'Spell packets. Ask around for advice on making these!',
+        'skills':{
+            "Clouded Memory": 1,
+            "Magical Aptitude": 1,
+            "Lore: Sorcery": 1,
+            "Mana Focus": 16,
+            "Sorcery": 1
+        }
+    },
+    'Practiced Surgeon':{
+        'description':"A master of healing arts. You can treat nearly any ailment that doesn't require magic.",
+        'required equipment':'Cloth strips for bandages. Safe, fake surgical implements. This can be as simple as a small foam knife and some thread.',
+        'skills':{
+            "Clouded Memory": 1,
+            "Lore: Anatomy": 1,
+            "Willpower": 1,
+            "Examine Wounds": 1,
+            "Detect Poison": 1,
+            "Apply Pressure": 1,
+            "Set Bone": 1,
+            "Administer Antidote": 2,
+            "Bandage": 1,
+            "Trauma Patch": 2,
+            "Surgery": 1,
+            "Sudden Motivation": 3
+        }
+    },
+    'Charismatic Courtier':{
+        'description':'A charismatic Noble who blends social and battlefield skills using wit, diplomacy, and generosity.',
+        'required equipment':'Sword up to 48"',
+        'skills':{
+            "Clouded Memory": 1,
+            "Oath Bound": 1,
+            "Nobility": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Salute": 1,
+            "Witty Repartee": 1,
+            "Dodge": 1,
+            "Willpower": 1,
+            "Dance Lesson": 1,
+            "Serenade": 1
+        }
+    },
+    'Alchemical Merchant':{
+        'description':'A merchant who makes their money by selling potions.',
+        'required equipment':'Potion bottle props(no glass).',
+        'skills':{
+            "Clouded Memory": 1,
+            "Lore: Alchemy": 1,
+            "Stable Alchemy": 3,
+            "Herbalism": 3,
+            "Mercantile": 2
+        }
+    },
+    'Dream of Potential':{
+        'description':'A little bit of everything.',
+        'required_equipment':'Sword up to 48"',
+        'skills':{
+            "Infinite Possibility": 1,
+            "Short Weapons": 1,
+            "One-Handed Weapons": 1,
+            "Toughness": 1,
+            "Dodge": 1,
+            "Stun": 1,
+            "Leap": 1,
+            "Drawn To The Muse": 1,
+            "Slumber Sight": 1
+        }
+    }
+}
