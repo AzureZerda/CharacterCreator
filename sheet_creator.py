bba04a3b2695366df630de337b32c660b9a18169
AzureFennec <carnauris@gmail.com>
2026-08-15 16:03:38 -0700

    Reverted

--- sheet_creator.py
+++ sheet_creator.py
@@ -412,6 +412,7 @@ def sheet_setup(sheet):
 
 def export_char(session):
     from google.oauth2.credentials import Credentials
+    print(session['google_credentials'])
     creds_data = session["google_credentials"]
     creds = Credentials(**creds_data)
     gc = gspread.authorize(creds)
